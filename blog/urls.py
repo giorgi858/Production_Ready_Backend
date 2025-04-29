75e49a9cb79d29efbb719ea3342713d4d04b8517
giorgi <giorgimtsituri5@gmail.com>
2025-04-29 11:46:30 +0400

    add urls settings

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from api.views import CreateUserView
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +14,12 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),   
     path('api/token/',TokenObtainPairView.as_view(), name='token'),
     path('api/token/refresh/',TokenRefreshView.as_view(), name='token-refresh'),  
-]
+] 
+
+# if settings.DEBUG:
+#     import debug_toolbar
+
+#     urlpatterns = [
+#         path('__debug__/', include(debug_toolbar.urls)),
+#     ] + urlpatterns
+    
